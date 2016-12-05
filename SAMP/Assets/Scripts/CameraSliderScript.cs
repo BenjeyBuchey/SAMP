c24361b4458fa059419633c4544c4c34a54b7b87
BenjeyBuchey <ch0b0@gmx.at>
2016-12-05 18:52:53 +0100

    added camera slider
    added camera slider
    small menu design changes

---
+++ SAMP/Assets/Scripts/CameraSliderScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSliderScript : MonoBehaviour {
+
+    public float vSliderValue = 80;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkZoom();
+	}
+
+    void OnGUI()
+    {
+        vSliderValue = GUI.VerticalSlider(new Rect(25, 50, 10, (Screen.height - 100)), vSliderValue, 100f, 60f);
+        
+    }
+
+    private void checkZoom()
+    {
+        Camera camera = gameObject.GetComponent<Camera>();
+        camera.fieldOfView = vSliderValue;
+    }
+}
