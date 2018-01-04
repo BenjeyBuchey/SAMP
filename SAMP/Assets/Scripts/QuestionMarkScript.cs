6cabb6ec69412c8d891964e38c939de9745ab907
BenjeyBuchey <ch0b0@gmx.at>
2018-01-04 11:37:13 +0100

    finished QuestionMark and PopupWindow

--- SAMP/Assets/Scripts/QuestionMarkScript.cs
+++ SAMP/Assets/Scripts/QuestionMarkScript.cs
@@ -5,22 +5,16 @@ using UnityEngine.EventSystems;
 
 public class QuestionMarkScript : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
-	public bool doWindow0 = true;
-	public Rect windowRect = new Rect(20, 20, 120, 50);
-	void OnGUI()
-	{
-		doWindow0 = GUI.Toggle(new Rect(10, 10, 100, 20), doWindow0, "Window 0");
-		if (doWindow0)
-			GUI.Window(0, new Rect(110, 10, 200, 60), DoMyWindow, "Basic Window");
-	}
-	void DoMyWindow(int windowID)
-	{
+	GameObject panel;
 
+	private void Start()
+	{
+		setPanel();
 	}
 
 	public void OnPointerClick(PointerEventData eventData)
 	{
-		Debug.Log("CLICKED ON QUESTION MARK!!!");
+		panel.SetActive(!panel.activeSelf);
 	}
 
 	public void OnPointerDown(PointerEventData eventData)
@@ -29,5 +23,16 @@ public class QuestionMarkScript : MonoBehaviour, IPointerClickHandler, IPointerD
 
 	public void OnPointerUp(PointerEventData eventData)
 	{
+	}
+
+	public void hidePanel()
+	{
+		panel.SetActive(false);
+	}
+
+	private void setPanel()
+	{
+		panel = this.transform.FindChild("PopupWindowPanel").gameObject;
+		panel.SetActive(false);
 	}
 }
