60e5593d612eaba2c4366b95a69d872ca10908d1
BenjeyBuchey <ch0b0@gmx.at>
2018-02-28 21:49:12 +0100

    fixed mergesort movement bug

--- SAMP/Assets/Scripts/MergeMoveScript.cs
+++ SAMP/Assets/Scripts/MergeMoveScript.cs
@@ -13,7 +13,6 @@ public class MergeMoveScript : MonoBehaviour {
 	Vector3 null_position;
 	Color prevColor, prevColor2;
 	private Text score;
-	private bool rotation = true;
     private float init_y = 0.0f;
 
 	// Use this for initialization
@@ -65,14 +64,13 @@ public class MergeMoveScript : MonoBehaviour {
 
 	private void getRotationPoint()
 	{
-		float distance = Mathf.Abs (dest2.z - go2.transform.position.z); //go1.transform.position.z
+		float distance = Mathf.Abs (dest2.z - go2.transform.position.z);
 		float z = 0.0f;
 		if (dest2.z > go2.transform.position.z)
 			z = dest2.z - distance / 2;
 		else
 			z = dest2.z + distance / 2;
 
-        //Debug.Log("INIT Y: " +init_y);
         rotationPoint = new Vector3(dest2.x,init_y,z);
 	}
 
@@ -93,7 +91,10 @@ public class MergeMoveScript : MonoBehaviour {
         {
 			int go1_id = 0, go2_id=0;
 			if (go1 != null)
+			{
 				old_positions[go1.GetComponent<SingleElementScript>().getElementId()] = null_position;
+				//Debug.Log("SET OLD [" + go1.GetComponent<SingleElementScript>().getElementId() + "] to NULL POS " + old_positions[go1.GetComponent<SingleElementScript>().getElementId()]);
+			}
 
 			rotated = 0;
             go1 = queue[i];
@@ -102,15 +103,16 @@ public class MergeMoveScript : MonoBehaviour {
 			changeColor(true);
 
             // changed 0 to init_y
-            if (go1.transform.position.y == init_y && go2.transform.position.y == init_y) 
-            {
+            if(Mathf.Abs(go1.transform.position.y - init_y) < 0.1f && Mathf.Abs(go2.transform.position.y - init_y) < 0.1f)	//if (go1.transform.position.y == init_y && go2.transform.position.y == init_y)
+			{
                 if (old_positions [go1.GetComponent<SingleElementScript> ().getElementId ()] == null_position) 
                 {
                     dest1 = go1.transform.position;
                     dest1.y += 10;
 
                     dest2 = go1.transform.position;
-                } else 
+                }
+				else 
                 {
 					// set dest2 to old_positions entry
                     dest2 = old_positions [go1.GetComponent<SingleElementScript> ().getElementId ()];
@@ -120,18 +122,19 @@ public class MergeMoveScript : MonoBehaviour {
                         dest1.y += 10;
                 }
                 old_positions [go2.GetComponent<SingleElementScript> ().getElementId ()] = go2.transform.position;
-                //Debug.Log ("SET OLD " + go2.GetComponent<SingleElementScript> ().getElementId ());
+                //Debug.Log ("SET OLD [" + go2.GetComponent<SingleElementScript> ().getElementId () + "] to " + old_positions[go2.GetComponent<SingleElementScript>().getElementId()]);
             }
 
-            if (go2.transform.position.y > init_y && go1.transform.position.y == init_y) 
-            {
+            if (go2.transform.position.y > init_y && Mathf.Abs(go1.transform.position.y - init_y) < 0.1f)    // go1.transform.position.y == init_y
+			{
                 if (old_positions [go1.GetComponent<SingleElementScript> ().getElementId ()] == null_position) 
                 {
                     dest2 = go1.transform.position;
 
                     dest1 = go1.transform.position;
                     dest1.y += 10;
-                } else 
+                }
+				else 
                 {
                     dest2 = old_positions [go1.GetComponent<SingleElementScript> ().getElementId ()];
                     dest1 = go1.transform.position;
@@ -140,15 +143,13 @@ public class MergeMoveScript : MonoBehaviour {
 
             getRotationPoint();
 
-            if (go2.transform.position.y > init_y) 
+            if ((go2.transform.position.y-0.1f) > init_y) 
             {
-                rotation = false;
-                //LeanTween.move(go2, dest2, 1.5f);
 				go2_id = LeanTween.move(go2, dest2, 1.5f).id;
+				//Debug.Log("GO2(" + go2.GetComponent<SingleElementScript>().getElementId() +"): FROM: " + go2.transform.position + " VIA: " + Vector3.zero + " TO: " + dest2);
 			} 
             else 
             {
-                rotation = true;
                 float y_offset = getOffsetY();
                 Vector3 temp1 = rotationPoint;
                 temp1.y = temp1.y + y_offset;
@@ -156,31 +157,17 @@ public class MergeMoveScript : MonoBehaviour {
                 Vector3 temp2 = rotationPoint;
                 temp2.y = temp2.y - y_offset;
 
-                //Debug.Log ("Temp1: " + temp1 +" Temp2: " + temp2);
-				
-				// TODO: check start -> temp -> dest
+				//Debug.Log("Rotation Point: " + rotationPoint + " Offset Y: " + y_offset);
+				//Debug.Log("GO2(" + go2.GetComponent<SingleElementScript>().getElementId() +"): FROM: " + go2.transform.position + " VIA: " + temp2 + " TO: " + dest2);
 
-                //LeanTween.move(go2, new Vector3[] {go2.transform.position, temp2, temp2, dest2 }, 1.5f);
 				go2_id = LeanTween.move(go2, new Vector3[] { go2.transform.position, temp2, temp2, dest2 }, 1.5f).id;
 			}
-                
-            //LeanTween.move(go1, dest1, 1.5f);
+			//Debug.Log("GO1(" + go1.GetComponent<SingleElementScript>().getElementId() +"): FROM: " + go1.transform.position + " TO: " + dest1);
+			//Debug.Log("-------------------------------------------------------");
 			go1_id = LeanTween.move(go1, dest1, 1.5f).id;
-
-			//Debug.Log ("From: " + go1.transform.position +" to: " + dest1);
-   //         Debug.Log ("From: " + go2.transform.position +" to: " + dest2);
-   //         Debug.Log ("Rotation Point: " + rotationPoint);
 
 			while (LeanTween.isTweening(go1_id) && LeanTween.isTweening(go2_id))
 				yield return null;
-
-			//while (Vector3.Distance(go1.transform.position, dest1) > 0.1f && Vector3.Distance(go2.transform.position, dest2) > 0.1f)
-			//	yield return null;
-
-			//while (go1.transform.position != dest1 || go2.transform.position != dest2)
-			//	yield return null;
-
-			//correctPositions();
 
             changeColor(false);
         }
